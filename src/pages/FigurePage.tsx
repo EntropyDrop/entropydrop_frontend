27f9d765104b5bcddaad7c6f9a30ea49056ade7a
EntropyDrop <fake_email@entropydrop.com>
2026-06-08 11:45:39 +0800

    style: rename Mono SLA option to SLA in bodyType options

--- src/pages/FigurePage.tsx
+++ src/pages/FigurePage.tsx
@@ -30,7 +30,7 @@ interface ForumComment {
 }
 
 const BODY_TYPES_ZH = ['单色光固化', 'fdm', '光固化喷墨打印 (UV Inkjet 3D Printing)'];
-const BODY_TYPES_EN = ['Mono SLA', 'FDM', 'UV Inkjet 3D Printing'];
+const BODY_TYPES_EN = ['SLA', 'FDM', 'UV Inkjet 3D Printing'];
 
 const MULTICOLOR_TYPES_ZH = ['贴纸', '3D全彩光固化', '2D全彩光固化', '喷涂', 'fdm多色'];
 const MULTICOLOR_TYPES_EN = ['Stickers', '3D Full-Color SLA', '2D Full-Color SLA', 'Spraying', 'FDM Multi-color'];
